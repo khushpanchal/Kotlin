1847e94e83554cfe0ef14b6beff29943f5be45a0
khushpanchal <khush.panchal123@gmail.com>
2023-07-22 15:32:08 +0530

    scope function (let,run,apply,also,with)

---
+++ app/src/main/java/com/example/kotlin/ScopeFunction.kt
@@ -0,0 +1,47 @@
+package com.example.kotlin
+
+class ScopeFunction {
+}
+
+class A(var name: String? = null)
+
+fun main() {
+
+    data class Person(var name: String = "Khush")
+    val personObject = Person()
+
+    val x = personObject.let {
+        it.name + " Panchal"
+    } //Return by lambda (Use personObject but can't modify)
+    println(x) // Khush Panchal
+    println(personObject.name) // Khush
+
+    val x1 = personObject.run {
+        this.name + " Panchal"
+    } //Return by lambda (lambda gets object itself, this) (Use personObject but can't modify)
+    println(x1) // Khush Panchal
+    println(personObject.name) // Khush
+
+
+    val y = personObject.also {
+        it.name = "Khush Panchal"
+        "returning random string" //unused
+    } // return personObject itself after lamda calling on personObject
+    println(y.name) // Khush Panchal
+    println(personObject.name) // Khush panchal
+
+    val y1 = personObject.apply {
+        this.name = "Khush Panchal 1"
+        "returning random string" //unused
+    } // return personObject itself after lamda calling on personObject (lambda gets object itself, this)
+    println(y.name) // Khush Panchal 1
+    println(personObject.name) // Khush Panchal 1
+
+    val z = with(personObject) {
+        this.name = "Khush Panchal 2"
+        "Return random string"
+    } //Return by lambda and also apply lambda on personObject
+    println(z) //"Return random string"
+    println(personObject.name) // Khush Panchal 2
+
+}
