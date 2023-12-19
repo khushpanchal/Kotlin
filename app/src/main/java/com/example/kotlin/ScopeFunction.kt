c6345f27ed7ee4845cc70e362ae6b9217c78ece6
khushpanchal <khush.panchal123@gmail.com>
2023-12-19 18:17:13 +0530

    scope function refactor

--- app/src/main/java/com/example/kotlin/ScopeFunction.kt
+++ app/src/main/java/com/example/kotlin/ScopeFunction.kt
@@ -1,52 +1,57 @@
 package com.example.kotlin
 
-class ScopeFunction {
-}
-
-class A(var name: String? = null)
+/**
+ * let   -  T.let(block: (T) -> R): R             ------- {it-> },         Returns last line
+ * run   -  T.run(block: T.() -> R): R  {}        ------- //object itself, Returns nothing
+ * also  - T.also(block: (T) -> Unit): T          ------- {it-> },         Returns last line
+ * apply - T.also(block: T.() -> Unit): T {}      ------- //object itself, Returns nothing
+ * with  - with(receiver: T, block: T.() -> R): R ------- //object itself, Returns last line
+ */
 
 fun main() {
 
     data class Person(var name: String = "Khush")
-    val personObject = Person()
+    val personObject = Person() //Khush
 
-    //block(this) (creates copy, so it is thread safe)
+    //(creates copy, so it is thread safe)
+    //Return block(this)
     val x = personObject.let {
         it.name + " Panchal"
-    } //Return by lambda (Use personObject but can't modify)
+    } //Return by lambda
     println(x) // Khush Panchal
     println(personObject.name) // Khush
 
 
-    //block() (Extension function, so use this)
+    //Return block() (Extension function, so use this) (lambda gets object itself, this)
     val x1 = personObject.run {
         this.name + " Panchal"
-    } //Return by lambda (lambda gets object itself, this) (Use personObject but can't modify)
+    } //Return by lambda (lambda gets object itself, this)
     println(x1) // Khush Panchal
     println(personObject.name) // Khush
 
 
-    //this
+    //Return this
     val y = personObject.also {
         it.name = "Khush Panchal"
         "returning random string" //unused
-    } // return personObject itself after lamda calling on personObject
+    }
     println(y.name) // Khush Panchal
     println(personObject.name) // Khush panchal
 
 
-    //this
+    //Return this (Extension function, so use this) (lambda gets object itself, this)
     val y1 = personObject.apply {
         this.name = "Khush Panchal 1"
         "returning random string" //unused
-    } // return personObject itself after lamda calling on personObject (lambda gets object itself, this)
-    println(y.name) // Khush Panchal 1
+    }
+    println(y1.name) // Khush Panchal 1
     println(personObject.name) // Khush Panchal 1
 
+    //Return block() (Extension function, so use this)
     val z = with(personObject) {
         this.name = "Khush Panchal 2"
         "Return random string"
-    } //Return by lambda and also apply lambda on personObject
+    }
     println(z) //"Return random string"
     println(personObject.name) // Khush Panchal 2
 
