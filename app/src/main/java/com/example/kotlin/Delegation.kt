39776ff2386910427ead40a4d4c900a3dded58d4
khushpanchal <khush.panchal123@gmail.com>
2023-12-19 18:35:19 +0530

    Delegation

--- app/src/main/java/com/example/kotlin/Delegation.kt
+++ app/src/main/java/com/example/kotlin/Delegation.kt
@@ -1,12 +1,17 @@
 package com.example.kotlin
 
-class Delegation
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
 
+/**
+ * Delegation in Kotlin: Passing the responsibility to other class or method
+ */
+//interface
 interface Action {
     fun eat()
     fun breathe()
 }
-
+//Animal implements action
 class Animal(val name: String): Action {
     override fun eat() {
         println("$name eats")
@@ -15,35 +20,63 @@ class Animal(val name: String): Action {
         println("$name breathes")
     }
 }
-
+//Bird implements action, but implementation is same as other animals, delegate to implementation details to Animal
 class Bird(private val name: String): Action by Animal(name){
     fun fly() {
         println("$name flies")
     }
 }
-
+//Same as Bird, have to implement Action, but same as other animal, so delegate
 class AquaticAnimal(private val name: String): Action by Animal(name){
     fun swim() {
         println("$name swims")
     }
 }
 
+//Other example
 interface ApplyOnce {
     fun getAppliedOnce()
 }
-
 class IapplyInterface(): ApplyOnce {
     override fun getAppliedOnce() {
         println("I have applied once")
     }
 }
-
-class LetsTryApplyInterface(): ApplyOnce by IapplyInterface(){
-
+class LetsTryApplyInterface: ApplyOnce by IapplyInterface(){
 }
 
 
+
+/**
+ * Property Delegation: Delegate Property (pass responsibility of getter and setter) to other class
+ */
+class Name {
+    constructor(firstName: String?, lastName: String?) {
+        this.firstName = firstName
+        this.lastName = lastName
+    }
+    var firstName: String? by NameDelegate()
+    var lastName: String? by NameDelegate()
+}
+
+class NameDelegate : ReadWriteProperty<Any?, String?> {
+    var formattedVal: String? = null
+    override fun getValue(thisRef: Any?, property: KProperty<*>): String? {
+        return formattedVal
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: String?) {
+        if(!value.isNullOrEmpty()) {
+            formattedVal = value
+        }
+    }
+
+
+}
 fun main() {
+    /**
+     * Delegation
+     */
     val parrot = Bird("Parrot")
     parrot.eat()
     parrot.breathe()
@@ -55,5 +88,11 @@ fun main() {
     fish.swim()
 
     LetsTryApplyInterface().getAppliedOnce()
+
+    /**
+     * Property Delegation
+     */
+    val lastName = Name("Khush", "Panchal").lastName
+    println(lastName)
 
 }
